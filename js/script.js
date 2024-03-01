c88916ed8a495f03a4b28c4931b97b4037982fc4
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-03-02 00:31:28 +0600

    final commit

--- js/script.js
+++ js/script.js
@@ -53,12 +53,18 @@ function displayMobileCard(phones) {
     div.classList = `p-6 border shadow-md rounded-lg flex flex-col text-center items-center dark:border-slate-800 dark:bg-slate-900`;
     div.innerHTML = `
                     <div class="w-full flex justify-center bg-slate-100 py-7 px-2 pb-12 rounded-lg dark:bg-slate-800">
-                      <img src="${phone.image}" alt="Product Picture" class="w-4/6" />
+                      <img src="${
+                        phone.image || '../media/product.png'
+                      }" alt="Product Picture" class="w-4/6" />
                     </div>
-                    <h3 class="text-2xl font-bold text-black mt-6 mb-2 dark:text-white min-h-16">${phone.brand} ${phone.phone_name}</h3>
+                    <h3 class="text-2xl font-bold text-black mt-6 mb-2 dark:text-white min-h-16">${
+                      phone.brand
+                    } ${phone.phone_name}</h3>
                     <p class="text-base font-normal text-[#706F6F] dark:text-slate-300 mb-3 min-h-24">There are many variations of passages of available, but the majority have suffered</p>
                     <h4 class="text-2xl font-bold text-black mb-4 dark:text-white">$999</h4>
-                    <button id="${phone.slug}" class="phone-details bg-[#0D6EFD] text-white md:text-lg font-semibold px-4 py-2 md:px-8 md:py-3 rounded-md hover:bg-opacity-90 duration-150 ease-in mb-2">Show Details</button>
+                    <button id="${
+                      phone.slug
+                    }" class="phone-details bg-[#0D6EFD] text-white md:text-lg font-semibold px-4 py-2 md:px-8 md:py-3 rounded-md hover:bg-opacity-90 duration-150 ease-in mb-2">Show Details</button>
                      `;
 
     ProductContainerElement.appendChild(div);
@@ -108,7 +114,7 @@ function displayDetails(data) {
                     <div class="flex flex-col justify-center items-start text-left">
                       <img
                       class="w-4/6 md:w-2/6 mb-4 self-center"
-                      src="${data.image}"
+                      src="${data.image || '../media/product.png'}"
                       alt="{phone.name}"
                       />
                       <h3 class="font-bold text-lg pb-6">${
